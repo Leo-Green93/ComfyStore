40b9e72baeabf5a80e1d4103ae4e750a837760ca
Leo Green <139697327+Leo-Green93@users.noreply.github.com>
2024-06-06 17:52:44 -0400

    First Commit

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import { configureStore } from "@reduxjs/toolkit";
+
+import cartReducer from "./features/cart/CartSlice";
+import userReducer from "./features/user/userSlice";
+
+export const store = configureStore({
+  reducer: {
+    cartState: cartReducer,
+    userState: userReducer,
+  },
+});
